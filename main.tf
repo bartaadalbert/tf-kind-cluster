fa577443e2f73f362f34e46d486c6a3dbac2dedd
Adalbert Barta <adalbertbarta@gmail.com>
2023-06-13 19:31:31 +0000

    base64 decode keys

--- main.tf
+++ main.tf
@@ -47,18 +47,14 @@ resource "null_resource" "extract_kubeconfig_values" {
 
   provisioner "local-exec" {
     command = <<-EOT
-      if kind get clusters | grep -q "${var.KIND_CLUSTER_NAME}"; then
-        if [ -f "${path.module}/kind-config" ]; then
-          KUBECONFIG=${path.module}/kind-config kubectl config use-context kind-${var.KIND_CLUSTER_NAME} &&
-          KUBECONFIG=${path.module}/kind-config kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[0].cluster.certificate-authority-data}' > ${path.module}/kind-ca &&
-          KUBECONFIG=${path.module}/kind-config kubectl config view --raw --minify --flatten -o jsonpath='{.users[0].user.client-certificate-data}' > ${path.module}/kind-crt &&
-          KUBECONFIG=${path.module}/kind-config kubectl config view --raw --minify --flatten -o jsonpath='{.users[0].user.client-key-data}' > ${path.module}/kind-client-key &&
-          KUBECONFIG=${path.module}/kind-config kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[0].cluster.server}' > ${path.module}/kind-endpoint
-        else
-          echo "${path.module}/kind-config does not exist."
-        fi
+      if [ -f "${path.module}/kind-config" ]; then
+        KUBECONFIG=${path.module}/kind-config kubectl config use-context kind-${var.KIND_CLUSTER_NAME} &&
+        KUBECONFIG=${path.module}/kind-config kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[0].cluster.certificate-authority-data}' | base64 --decode > ${path.module}/kind-ca &&
+        KUBECONFIG=${path.module}/kind-config kubectl config view --raw --minify --flatten -o jsonpath='{.users[0].user.client-certificate-data}' | base64 --decode > ${path.module}/kind-crt &&
+        KUBECONFIG=${path.module}/kind-config kubectl config view --raw --minify --flatten -o jsonpath='{.users[0].user.client-key-data}' | base64 --decode > ${path.module}/kind-client-key &&
+        KUBECONFIG=${path.module}/kind-config kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[0].cluster.server}' > ${path.module}/kind-endpoint
       else
-        echo "Cluster ${var.KIND_CLUSTER_NAME} does not exist."
+        echo "${path.module}/kind-config does not exist."
       fi
     EOT
     interpreter = ["bash", "-c"]
