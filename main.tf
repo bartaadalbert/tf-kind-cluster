0dd2b7308e5b67d0199869d24362e369509260c6
Adalbert Barta <adalbertbarta@gmail.com>
2023-06-13 18:35:44 +0000

    Add certificate files as outputs in tf-kind-cluster module

--- main.tf
+++ main.tf
@@ -47,19 +47,24 @@ resource "null_resource" "extract_kubeconfig_values" {
 
   provisioner "local-exec" {
     command = <<-EOT
-      if [ -f "${path.module}/kind-config" ]; then
-        KUBECONFIG=${path.module}/kind-config kubectl config use-context kind-${var.KIND_CLUSTER_NAME} &&
-        KUBECONFIG=${path.module}/kind-config kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[0].cluster.certificate-authority-data}' > ${path.module}/kind-ca &&
-        KUBECONFIG=${path.module}/kind-config kubectl config view --raw --minify --flatten -o jsonpath='{.users[0].user.client-certificate-data}' > ${path.module}/kind-crt &&
-        KUBECONFIG=${path.module}/kind-config kubectl config view --raw --minify --flatten -o jsonpath='{.users[0].user.client-key-data}' > ${path.module}/kind-client-key &&
-        KUBECONFIG=${path.module}/kind-config kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[0].cluster.server}' > ${path.module}/kind-endpoint
+      if kind get clusters | grep -q "${var.KIND_CLUSTER_NAME}"; then
+        if [ -f "${path.module}/kind-config" ]; then
+          KUBECONFIG=${path.module}/kind-config kubectl config use-context kind-${var.KIND_CLUSTER_NAME} &&
+          KUBECONFIG=${path.module}/kind-config kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[0].cluster.certificate-authority-data}' > ${path.module}/kind-ca &&
+          KUBECONFIG=${path.module}/kind-config kubectl config view --raw --minify --flatten -o jsonpath='{.users[0].user.client-certificate-data}' > ${path.module}/kind-crt &&
+          KUBECONFIG=${path.module}/kind-config kubectl config view --raw --minify --flatten -o jsonpath='{.users[0].user.client-key-data}' > ${path.module}/kind-client-key &&
+          KUBECONFIG=${path.module}/kind-config kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[0].cluster.server}' > ${path.module}/kind-endpoint
+        else
+          echo "${path.module}/kind-config does not exist."
+        fi
       else
-        echo "${path.module}/kind-config does not exist."
+        echo "Cluster ${var.KIND_CLUSTER_NAME} does not exist."
       fi
     EOT
     interpreter = ["bash", "-c"]
   }
 }
+
 
 
 resource "null_resource" "get_clusters" {
