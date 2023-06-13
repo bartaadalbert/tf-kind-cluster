96add8de9e93d2843480b3b24d5e06d3b6ff2f96
Adalbert Barta <adalbertbarta@gmail.com>
2023-06-13 17:23:58 +0000

    Add certificate files as outputs in tf-kind-cluster module

--- outputs.tf
+++ outputs.tf
@@ -20,6 +20,6 @@ output "crt" {
 
 output "endpoint" {
   description = "The endpoint for the created cluster."
-  # value       = ""
-  value       = "${path.module}/kind-endpoint"
+  value       = "https://127.0.0.1:45141"
+  # value       = "${path.module}/kind-endpoint"
 }
