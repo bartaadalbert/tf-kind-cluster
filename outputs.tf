16bfdb83412f28ac449912f0e844e885649008a3
Adalbert Barta <adalbertbarta@gmail.com>
2023-06-13 16:04:40 +0000

    Add certificate files as outputs in tf-kind-cluster module

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,19 @@ output "kubeconfig" {
   description = "The kubeconfig for the created cluster."
   value       = "${path.module}/kind-config"
 }
+
+output "client_key" {
+  value = null_resource.get_kubeconfig.*.triggers.client_key[0]
+}
+
+output "ca" {
+  value = null_resource.get_kubeconfig.*.triggers.ca[0]
+}
+
+output "crt" {
+  value = null_resource.get_kubeconfig.*.triggers.crt[0]
+}
+
+output "endpoint" {
+  value = null_resource.get_kubeconfig.*.triggers.endpoint[0]
+}
