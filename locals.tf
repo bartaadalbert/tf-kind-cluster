2cb32d9a790f53ce9b0463634eb77ce547365a8e
Adalbert Barta <adalbertbarta@gmail.com>
2023-06-13 08:06:09 +0000

    workers indexes,no

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
   master_nodes = [for i in range(var.NUM_MASTERS) : { role = "control-plane" }]
-  worker_nodes = [for i in range(var.NUM_WORKERS) : { role = "worker${i+1}" }]
+  worker_nodes = [for i in range(var.NUM_WORKERS) : { role = "worker" }]
   all_nodes    = concat(local.master_nodes, local.worker_nodes)
 }
