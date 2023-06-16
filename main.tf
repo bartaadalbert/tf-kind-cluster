cf9cdd90bbc887505462d5d0ca761af8e0a6a8e2
Adalbert Barta <adalbertbarta@gmail.com>
2023-06-16 15:11:43 +0200

    changed outputs making

--- main.tf
+++ main.tf
@@ -88,26 +88,17 @@ resource "null_resource" "extract_kubeconfig_values" {
           client_key_data=$(KUBECONFIG=${path.module}/kind-config kubectl config view --raw --minify --flatten -o jsonpath='{.users[0].user.client-key-data}' | base64 --decode)
           server=$(KUBECONFIG=${path.module}/kind-config kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[0].cluster.server}')
 
-          output "client_key" {
-            description = "The client key for the created cluster."
-            value       = client_key_data
-            sensitive   = true
-          }
+          echo "cluster_ca_data=$cluster_ca_data" > ${path.module}/kind-c.crt
+          echo "client_crt_data=$client_crt_data" > ${path.module}/kind-crt.crt
+          echo "client_key_data=$client_key_data" > ${path.module}/kind-client-key.pem
+          echo "server=$server" > ${path.module}/kind-endpoint
 
-          output "ca" {
-            description = "The CA certificate for the created cluster."
-            value       = cluster_ca_data
-          }
+          echo "Cluster CA Data: $cluster_ca_data"
+          echo "Client Certificate Data: $client_crt_data"
+          echo "Client Key Data: $client_key_data"
+          echo "Server: $server"
 
-          output "crt" {
-            description = "The client certificate for the created cluster."
-            value       = client_crt_data
-          }
-
-          output "endpoint" {
-            description = "The endpoint for the created cluster."
-            value       = server
-          }
+          echo "{\"cluster_ca_data\": \"$cluster_ca_data\", \"client_crt_data\": \"$client_crt_data\", \"client_key_data\": \"$client_key_data\", \"server\": \"$server\"}" > ${path.module}/kind-config-values
         else
           echo "${path.module}/kind-config does not exist."
         fi
