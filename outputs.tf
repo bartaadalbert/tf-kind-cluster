5dec1352a9fb0fb3425ec2ddc2908a6c4d66cb34
Adalbert Barta <adalbertbarta@gmail.com>
2023-06-16 15:13:01 +0200

    changed outputs making

--- outputs.tf
+++ outputs.tf
@@ -3,45 +3,23 @@ output "kubeconfig" {
   value       = fileexists("${path.module}/kind-config") ? file("${path.module}/kind-config") : "File not found"
 }
 
-# output "client_key" {
-#   description = "The client key for the created cluster."
-#   value       = fileexists("${path.module}/kind-client-key.pem") ? file("${path.module}/kind-client-key.pem") : "File not found"
-#   sensitive   = true
-# }
-
-# output "ca" {
-#   description = "The CA certificate for the created cluster."
-#   value       = fileexists("${path.module}/kind-ca.crt") ? file("${path.module}/kind-ca.crt") : "File not found"
-# }
-
-# output "crt" {
-#   description = "The client certificate for the created cluster."
-#   value       = fileexists("${path.module}/kind-crt.crt") ? file("${path.module}/kind-crt.crt") : "File not found"
-# }
-
-# output "endpoint" {
-#   description = "The endpoint for the created cluster."
-#   value       = fileexists("${path.module}/kind-endpoint") ? file("${path.module}/kind-endpoint") : "https://127.0.0.1:45141"
-# }
 output "client_key" {
   description = "The client key for the created cluster."
-  value       = null_resource.extract_kubeconfig_values.client_key
+  value       = fileexists("${path.module}/kind-client-key.pem") ? file("${path.module}/kind-client-key.pem") : "File not found"
   sensitive   = true
 }
 
 output "ca" {
   description = "The CA certificate for the created cluster."
-  value       = null_resource.extract_kubeconfig_values.ca
+  value       = fileexists("${path.module}/kind-ca.crt") ? file("${path.module}/kind-ca.crt") : "File not found"
 }
 
 output "crt" {
   description = "The client certificate for the created cluster."
-  value       = null_resource.extract_kubeconfig_values.crt
+  value       = fileexists("${path.module}/kind-crt.crt") ? file("${path.module}/kind-crt.crt") : "File not found"
 }
 
 output "endpoint" {
   description = "The endpoint for the created cluster."
-  value       = null_resource.extract_kubeconfig_values.endpoint
+  value       = fileexists("${path.module}/kind-endpoint") ? file("${path.module}/kind-endpoint") : "https://127.0.0.1:45141"
 }
-
-
